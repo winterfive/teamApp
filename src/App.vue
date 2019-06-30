93fb001593091c81871bc4a74f3974ff0d776cd3
Lee Gainer <piealacode@gmail.com>
2019-06-30 11:26:38 -0400

    Added fetch, axios package

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,38 @@
 <template>
   <div id="app">
     <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
   </div>
 </template>
 
 <script>
 import Card from "./components/Card.vue";
+import axios from 'axios';
 
 export default {
   name: "app",
   components: {
     Card
+  },
+  data: function() {
+    return {
+      playerArray: null
+    }
+  },
+  created() {
+    axios.get(`https://dii-test.s3.amazonaws.com/players.json`)
+    .then(response => {
+      this.playerArray = response.data
+      console.log(this.playerArray[3].name)
+    })
+    .catch(e => {
+      console.log("get error: " + e)
+    })
   }
 };
+
 </script>
 
 <style lang="scss">
@@ -23,5 +43,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
 }
 </style>
